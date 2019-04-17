a36e9d45ff88e4c31dd99702297b6f48417f76b2
Manos Gerothanasis <pli1723@gmail.com>
2019-04-17 21:35:35 +0300

    American Volcanoes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Mapping_with_Python
+adding Markers whith Volcanoes in America
--- map.py
+++ map.py
@@ -1,10 +1,17 @@
+import pandas
 import folium
-map = folium.Map(location=[40.63,22.94],zoom_start = 10)
 
-fg = folium.FeatureGroup(name = "My feauter")
+map = folium.Map([40.63,22.94],zoom_start = 3)
 
-for condinates in [[40.631,22.942],[40.64,22.943],[55.64,66.943]]:
-    fg.add_child(folium.Marker(location=condinates, popup ="Hi I am a Marker", icon = folium.Icon(color = "green")))
+fg =folium.FeatureGroup(name = "My feauter")
+try:
+    data = pandas.read_csv("Volcanoes.txt")
+except:
+    data = False
+    
+for intex , row  in data.iterrows() :
+        fg.add_child(folium.Marker(location=[row["LAT"],row["LON"]], popup =row["NAME"], icon = folium.Icon(color = "red")))
+
 map.add_child(fg)
 
 map.save("Map.html")
