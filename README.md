69b8f7a5ea9b678a8ee981d6c6b38c05e4d3114a
Manos Gerothanasis <pli1723@gmail.com>
2019-04-17 22:27:49 +0300

    American Volcanoes and countries

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Mapping_with_Python
-<<<<<<< HEAD
-adding Markers whith Volcanoes in America
-=======
-Is a python programm makes an html file 
-html contain all world map
->>>>>>> 1cbffeb524727efc80585d421ae893fa00b6b8f0
+adding CircleMarkers whith Volcanoes in all the World
+different color for different Elevation
--- map.py
+++ map.py
@@ -1,17 +1,52 @@
 import pandas
 import folium
+import numpy
 
-map = folium.Map([40.63,22.94],zoom_start = 3)
 
-fg =folium.FeatureGroup(name = "My feauter")
+
+map = folium.Map([40.63,22.94],zoom_start = 2)
+
+fg_v =folium.FeatureGroup(name = "Volcanoes")
+fgp =folium.FeatureGroup(name = "Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json',
+                                      'r',
+                                      encoding = 'utf-8-sig').read(),
+                                      style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000
+                                      else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' } ))
 try:
-    data = pandas.read_csv("Volcanoes.txt")
+    data = pandas.read_csv("ALL World's volcanoes.csv", sep=";")
+    
 except:
     data = False
-    
-for intex , row  in data.iterrows() :
-        fg.add_child(folium.Marker(location=[row["LAT"],row["LON"]], popup =row["NAME"], icon = folium.Icon(color = "red")))
+latL  = list(data["Lat"])
+lonL  = list(data["Lon"])
+eleL  = list(data["Elevation (m)"])
+nameL = list(data["Volcano Name"])
 
-map.add_child(fg)
+def getColorByEle(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000 :
+        return "orange"
+    else :
+        return "red"
+    
+
+
+for lat,lon,ele,name  in zip(latL,lonL,eleL,nameL) :
+    pop = "Volcano Name: {} \nElevation: {} (m)".format(name,ele)
+    fg_v.add_child(folium.CircleMarker(location=[lat,lon],
+                                     radius = 7,
+                                     popup = pop ,
+                                     fill_color=getColorByEle(ele),
+                                     fill = True,
+                                     color = "grey",
+                                     fill_opacity = 0.7    ))
+    
+map.add_child(fgp)
+map.add_child(fg_v)
+
+map.add_child(folium.LayerControl())
 
 map.save("Map.html")
